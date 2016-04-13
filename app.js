098adbfb42e36c16cb3e91076f155c91194adf9a
Taylor Kennedy <taykenned@gmail.com>
2016-04-13 10:12:29 -1000

    adds require for articles and products routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var pug = require('pug');
 var app = express();
 
+var productRoute = require('./routes/products');
+var articlesRoute = require('./routes/articles');
+
 app.set('view engine', pug);
 app.set('views', './views');
 
@@ -12,4 +15,11 @@ app.get('/', function(req, res) {
   res.json({success : true});
 });
 
-app.listen(3000);
+app.use('/products', productRoute);
+app.use('/articles', articlesRoute);
+
+if(!module.parent) {
+  app.listen(3000);
+}
+
+module.exports = app;
