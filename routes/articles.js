264a1897111db4a200e290d03a6014929ab4c491
Natalie Macias <nramirez787@gmail.com>
2016-04-13 20:39:33 -1000

    adding templates and rendering functions for articles and products

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var databaseHelper = require('../db/databaseHelper');
 
+var articles = databaseHelper.model('article', {
+  title: { type: 'string', id: true},
+  body: { type: 'string'},
+  author: { type: 'string'},
+  urlTitle: { type: 'string'}
+});
 
 router.get('/:title/edit', function(req, res) {
 
 });
 
 router.get('/new', function(req, res) {
-
+  res.render('new', {
+    redirect: '/articles',
+    inputs: ['title', 'author', 'body']
+   });
 });
 
 router.route('/:title')
@@ -19,11 +29,17 @@ router.route('/:title')
   });
 
 router.route('/')
-  .post(function(req, res) {
-
+ .post(function(req, res) {
+    articles.add({
+      title: req.body.title,
+      author: req.body.author,
+      body: req.body.body,
+    });
+    res.redirect('/articles');
   })
   .get(function(req, res) {
-
+    res.render('index', { header: 'Articles', list: articles.all() });
   });
+
 
 module.exports = router;
